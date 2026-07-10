1e9e9073db7a40e06a1c93b022e32af0bb126b02
ab-md <metafite800@gmail.com>
2026-07-10 19:45:30 +0330

    Fix sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,8 +1,10 @@
 export default function sitemap() {
   return [
     {
-      url: "https://portfolio-tau-peach-gnvigacep1.vercel.app",
+      url: "https://portfolio-tau-peach-gnvigacep1.vercel.app/",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
   ];
 }
